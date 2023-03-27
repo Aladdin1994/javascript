39c4a1d577296db3a7d7d06a04c20a4bf9f3d2ae
aladdin1994 <aladdinis@cod.edu.az>
2023-03-28 00:17:01 +0400

    2

---
+++ script.js
@@ -0,0 +1,91 @@
+// let const var
+
+// let x=10;
+
+// x=30;
+
+// let groupName = 'P133';
+
+// groupName='23'
+
+// console.log(groupName);
+
+// var groupName ='p231'
+
+// groupName='213'
+
+// console.log(groupName);
+
+// let a = 'let';
+
+// var aa='var'
+
+// console.log(this);
+
+// if(true){
+//     let x=10;
+//     console.log(x);
+// }
+
+// const groupName = 'P231';
+// groupName='1223'
+// console.log(groupName);
+
+// let groupName=`DS`;
+
+// console.log('CODE ACADEMY'+" "+ groupName);
+
+
+// console.log(`Code acac ${groupName}`);
+
+// let x =null;
+  
+// let x ='21';
+
+// let y='2'
+
+// let c=x-y;
+// console.log(c);
+
+// let x=32;
+// let y=2;
+
+
+
+// alert(x<y);
+
+
+// let ans=prompt('sdasd ' ,'asdas');
+// console.log(ans);
+
+// let a=confirm('asdasdasdasda?')
+// console.log(a);
+
+// let x =6;
+
+// console.log(++x);
+
+
+
+// let a=prompt('dsadasdsadd');
+// let c='3'
+
+// let result=(a==c)?'sdad': (a>1 && a<4)?'asdsadasd':'hgfh';
+// alert(result);
+
+// if(a=='3'){
+// alert('desad')
+// }
+// else{
+//     alert('aszadas')
+// }
+
+
+let num = prompt('enter number')
+
+
+if(num>0){
+    console.log("positive");
+}else{
+    console.log("negative");
+}
